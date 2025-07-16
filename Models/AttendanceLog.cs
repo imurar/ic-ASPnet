6f532b32783b383ac72e1888194db086e02d4fb8
imurar <imurar@outlook.com>
2025-07-16 18:30:10 +0900

    feat: crete-AttendanceLog-model

---
+++ Models/AttendanceLog.cs
@@ -0,0 +1,11 @@
+﻿namespace ic_ASPnet.Models
+{
+    public class AttendanceLog
+    {
+    public DateTime TimeStamp { get; set; }
+    public required string Name { get; set; }
+    public required string CardUid { get; set; }
+    public required string Type { get; set; } // "IN" or "OUT"
+
+    }
+}
